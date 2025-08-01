374a94866b8e8b1cee830051dd6168739b877c6f
Rohit Rajendra Pokalwar <rohitpokalwar95@gmail.com>
2025-08-01 18:17:15 +0530

    Delete models directory

--- models/clinic.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ClinicSchema = new mongoose.Schema({
-  name: String,
-  address: String
-});
-
-module.exports = mongoose.model('Clinic', ClinicSchema);
